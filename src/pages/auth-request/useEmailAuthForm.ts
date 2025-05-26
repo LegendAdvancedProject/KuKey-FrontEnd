fb2134c6d4aa951629f4b973ac5b008c9bc2e1b0
jstar_00 <wltjd6300@naver.com>
2025-05-26 11:57:30 +0900

    refactor: 학생 인증 폼 리팩토링
    - 인증로직 및 상태관리 코드 커스텀 훅으로 분리
    - email과 authcode를 두 개의 form으로 분리
    - 인증코드 검증 api 구현(아직 테스트 x)

---
+++ src/pages/auth-request/useEmailAuthForm.ts
@@ -0,0 +1,79 @@
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { useState } from 'react';
+import { requestAuthCode, verifyAuthCode } from '../../shared/apis/auth/auth';
+
+export const useEmailAuthForm = () => {
+  // 이메일 입력 폼
+  const {
+    register: registerEmail,
+    handleSubmit: handleSubmitEmail,
+    watch: watchEmail,
+    formState: { errors: errorsEmail, isValid: isValidEmail },
+  } = useForm<{ userEmail: string }>({ mode: 'onChange' });
+
+  // 인증번호 입력 폼
+  const {
+    register: registerAuth,
+    handleSubmit: handleSubmitAuth,
+    watch: watchAuth,
+    formState: { errors: errorsAuth },
+  } = useForm<{ authNumber: number }>({ mode: 'onChange' });
+
+  const [showEnterAuth, setShowEnterAuth] = useState(false);
+
+  const userEmail = watchEmail('userEmail');
+  const authNumber = watchAuth('authNumber');
+
+  // 이메일 제출 처리
+  const onSubmitEmail: SubmitHandler<{ userEmail: string }> = async (data) => {
+    console.log('이메일 제출 성공!', data);
+    try {
+      const response = await requestAuthCode(userEmail);
+      console.log(response);
+      if (!response.data.isVerified) {
+        setShowEnterAuth(true); // 인증번호 입력 필드 보여주기
+      } else {
+        // 인증정보 기억돼있으면 바로 개방요청 처리 가능
+      }
+    } catch {
+      alert('인증번호 요청 오류가 발생했습니다.');
+    }
+  };
+
+  // 인증번호 제출 처리
+  const onSubmitAuth: SubmitHandler<{ authNumber: number }> = async (data) => {
+    console.log('인증번호 제출 성공!', data);
+    console.log('인증정보 확인 요청됨');
+    try {
+      const response = await verifyAuthCode(userEmail, authNumber);
+      if (response.code === 200) {
+        // 인증 성공, accessToken 저장 등 처리
+      }
+    } catch {
+      alert('인증정보 확인 오류가 발생했습니다.');
+    }
+  };
+
+  const handleBack = () => setShowEnterAuth(false);
+
+  return {
+    // 이메일 관련
+    registerEmail,
+    handleSubmitEmail,
+    errorsEmail,
+    isValidEmail,
+    userEmail,
+    // 인증번호 관련
+    registerAuth,
+    handleSubmitAuth,
+    errorsAuth,
+    authNumber,
+    // 상태
+    showEnterAuth,
+    setShowEnterAuth,
+    // 액션
+    onSubmitEmail,
+    onSubmitAuth,
+    handleBack,
+  };
+};
