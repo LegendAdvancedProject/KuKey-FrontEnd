54a6801f55c16950334c52f5a015c6831c81eed4
jstar_00 <wltjd6300@naver.com>
2025-05-26 19:32:43 +0900

    chore: 불필요한 코드 제거

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -33,29 +33,6 @@ const Home = () => {
             />
           </div>
         ))}
-
-        {/* <div>
-          <Building buildingName="공학관" />
-          <div className="mt-[10px] flex w-full flex-col gap-[10px]">
-            <SpaceStatus spaceName="과방1" lockStatus="잠금" spaceId={1}></SpaceStatus>
-            <SpaceStatus spaceName="과방2" lockStatus="잠금" spaceId={2}></SpaceStatus>
-          </div>
-        </div>
-
-        <div>
-          <Building buildingName="신공학관" />
-          <div className="mt-[10px] flex w-full flex-col gap-[10px]">
-            <SpaceStatus spaceName="1013호" lockStatus="잠금" spaceId={3}></SpaceStatus>
-            <SpaceStatus spaceName="1014호" lockStatus="잠금" spaceId={4}></SpaceStatus>
-          </div>
-        </div>
-
-        <div>
-          <Building buildingName="새천년관" />
-          <div className="mt-[10px] flex w-full flex-col gap-[10px]">
-            <SpaceStatus spaceName="501호" lockStatus="잠금" spaceId={5}></SpaceStatus>
-          </div>
-        </div> */}
       </div>
     </div>
   );
